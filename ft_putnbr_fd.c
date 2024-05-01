5b37e63926dceee3eb4b664cb8520b72cc3c2d1b
Theau De La Ruelle <tde-la-r@2G1.42angouleme.fr>
2024-05-01 15:52:13 +0200

    write putnbr opti

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,25 @@
 /*   By: tde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 20:33:16 by tde-la-r          #+#    #+#             */
-/*   Updated: 2023/11/17 17:25:42 by tde-la-r         ###   ########.fr       */
+/*   Updated: 2024/05/01 15:47:53 by tde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	get_number(unsigned int nbr, char to_print[10], int *i)
+{
+	if (nbr > 9)
+		get_number(nbr / 10, to_print, i);
+	to_print[*i] = nbr % 10 + '0';
+	(*i)++;
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
 	unsigned int	nbr;
-	char			c;
+	int				i;
+	char			to_print[10];
 
 	nbr = n;
 	if (n < 0)
@@ -23,8 +32,7 @@ void	ft_putnbr_fd(int n, int fd)
 		nbr = -n;
 		write(fd, "-", 1);
 	}
-	if (nbr > 9)
-		ft_putnbr_fd(nbr / 10, fd);
-	c = nbr % 10 + '0';
-	write(fd, &c, 1);
+	i = 0;
+	get_number(nbr, to_print, &i);
+	write(fd, to_print, i);
 }
