63eac05f9fa48e31b06712f80db7a89593c0617c
Theau De La Ruelle <tde-la-r@2G1.42angouleme.fr>
2024-05-01 16:26:04 +0200

    opti printf %s

--- sources/printf_args.c
+++ sources/printf_args.c
@@ -6,7 +6,7 @@
 /*   By: tde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:05:18 by tde-la-r          #+#    #+#             */
-/*   Updated: 2024/04/06 00:35:53 by tde-la-r         ###   ########.fr       */
+/*   Updated: 2024/05/01 16:23:51 by tde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,14 @@ static void	ft_putnbr_ptf(int fd, unsigned long number, char *base, int *count)
 void	print_string_ptf(int fd, va_list *list, int *count)
 {
 	char	*str;
+	size_t	len;
 
 	str = va_arg(*list, char *);
 	if (str)
 	{
-		while (*str)
-		{
-			write(fd, str, 1);
-			(*count)++;
-			str++;
-		}
+		len = ft_strlen(str);
+		write(fd, str, len);
+		*count += len;
 	}
 	else
 	{
